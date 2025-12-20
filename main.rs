f6a4fbecc08ca2279768bf9b01241e4b5e8229e0
Raghumani Mehta <raghumanimehta@dhcp-206-87-192-220.ubcsecure.wireless.ubc.ca>
2025-12-20 01:37:42 -0800

    added basic inode struct

--- main.rs
+++ main.rs
@@ -1,9 +1,28 @@
 use fuser::{Filesystem, MountOption};
 use std::env;
+use std::fs::FileType;
 
 struct NullFS;
 
 impl Filesystem for NullFS {}
+
+const BLOCK_SIZE: u64 = 4096;
+const NUM_DIRECT_PTR: usize = 12;
+
+pub struct Inode {
+    pub ino_id: u64,            // inode number
+    pub size: u64,              // file size 
+    pub blocks: u64,            // num blocks allocated 
+    pub mtime_secs: i64,        // Easier to save to disk than SystemTime. Ignored the atime and ctime for now. 
+    pub kind: FileType,  
+    pub perm: u16, 
+    pub direct_blks: [u32; NUM_DIRECT_PTR], 
+    pub indirect_blk: u32,
+    pub dbl_indirect_blk: u32,
+    pub tri_indirect_blk: u32, 
+}
+
+
 
 fn main() {
     env_logger::init();
