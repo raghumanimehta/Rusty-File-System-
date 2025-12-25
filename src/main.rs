f43331b8835a7775c21f495a72c6df057a679f3e
Raghumani Mehta <raghumani2003@outlook.com>
2025-12-26 02:07:24 +0530

    the code compiles but the logic is a bit off, will continue tomorrow

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,15 @@ use bitvec::prelude::*;
 use fuser::{FileType, Filesystem, MountOption};
 use log::{debug, error, info, log_enabled, Level};
 use std::env;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 struct NullFS;
 impl Filesystem for NullFS {}
 
 // This is the total capacity of the backing storage for the file system
 // this includes the space used for the superblock, free object bitmaps, and file data and metadata
-const FS_SIZE_BYTES: u64 = 1u64 * (0b1 << 30) as u64; // 1 GB, toy size
-const BLK_SIZE_BYTES: u64 = 4096u64;
+const FS_SIZE_BYTES: u64 = 1u64 << 30; // 1 GB
+const BLK_SIZE_BYTES: u64 = 4096;
 const NUM_DATA_BLKS: u32 = (FS_SIZE_BYTES / BLK_SIZE_BYTES) as u32;
 const FREE_BLK_BMAP_SIZE_BYTES: usize = ((NUM_DATA_BLKS + 7) / 8) as usize;
 
@@ -17,6 +18,7 @@ const FREE_BLK_BMAP_SIZE_BYTES: usize = ((NUM_DATA_BLKS + 7) / 8) as usize;
 const MAX_NUM_INODES: u32 = 10; // toy size
 const FREE_INODE_BMAP_SIZE_BYTES: usize = ((MAX_NUM_INODES + 7) / 8) as usize;
 const NUM_INO_DIRECT_PTR: usize = 12;
+const INVALID_PTR: u32 = 0;
 
 // free inode bitmap can begin right after this struct and inode table can follow immediately after
 struct SuperBlock {
@@ -75,6 +77,30 @@ struct Inode {
     tri_indirect_blk: u32,
 }
 
+fn secs_from_unix_epoch() -> i64 {
+    SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .map(|d| d.as_secs() as i64)
+        .unwrap_or(0)
+}
+
+impl Inode {
+    fn new(ino_id: u64, kind: FileType, perm: u16) -> Self {
+        Self {
+            ino_id,
+            size: 0,
+            blocks: 0,
+            mtime_secs: secs_from_unix_epoch(),
+            kind,
+            perm,
+            direct_blks: [INVALID_PTR; NUM_INO_DIRECT_PTR],
+            indirect_blk: INVALID_PTR,
+            dbl_indirect_blk: INVALID_PTR,
+            tri_indirect_blk: INVALID_PTR,
+        }
+    }
+}
+
 #[derive(Default)]
 struct FreeInodeBitmap {
     map: BitArray<[u8; FREE_INODE_BMAP_SIZE_BYTES], Lsb0>,
@@ -91,13 +117,51 @@ struct FSState {
     superblk: SuperBlock,
     free_blks: FreeBlockBitmap,
     inode_bitmap: FreeInodeBitmap,
-    inodes: Vec<Inode>,
+    inodes: [Option<Inode>; MAX_NUM_INODES as usize],
     blks: Vec<u8>,
 }
 
+#[derive(Debug)]
+enum InodeError {
+    NoFreeInodesOnAlloc,
+    InodeNotFound,
+    InvalidInoId
+}
+
 impl FSState {
-    fn alloc_inode(&mut self) -> Option<u64> {
-        None
+    fn alloc_inode(&mut self, kind: FileType, perm: u16) -> Result<u64, InodeError> {
+        let ino_idx: usize = match self.inode_bitmap.find_first_free() {
+            Some(idx) => idx,
+            None => {
+                error!("alloc_inode failed: no free inodes");
+                return Err(InodeError::NoFreeInodesOnAlloc);
+            }
+        };
+
+        // mark inode allocated in bitmap
+        self.inode_bitmap.map.set(ino_idx, true);
+
+        // The true index is 0, but we use 0 as an invalid ptr so always 
+        // increment when storing ptrs 
+        let ino_id: u64 = (ino_idx + 1) as u64; 
+        self.inodes[ino_idx] = Some(Inode::new(ino_id, kind, perm));
+
+        Ok(ino_id)
+    }
+
+    fn fnid_inode_by_id(&self, ino_id: u64) -> Result<&Inode, InodeError> {
+        if ino_id <= 0 {
+            return Err(InodeError::InvalidInoId);
+        }
+        let ino_idx = (ino_id - 1) as usize;
+        if ino_idx >= (MAX_NUM_INODES as usize) {
+            return Err(InodeError::InodeNotFound);
+        }
+
+        match self.inodes[ino_idx].as_ref() {
+            Some(inode) => Ok(inode),
+            None => Err(InodeError::InodeNotFound),
+        } 
     }
 }
 
